5c2ccf81aa807ba9397204efe3f209ad7698ec41
jyotpanwar <jyot_panwar@yahoo.com>
2020-08-31 12:31:12 +0530

    Added iDialogService file

---
+++ Core/Service/IDialogService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Core.Service
+{
+    public interface IDialogService
+    {
+        void ShowMessage(string title, string message, string dismissButtonTitle, Action dismissed);
+
+        void Confirm(string title, string message, string okButtonTitle, string dismissButtonTitle, Action confirmed, Action dismissed);
+    }
+
+    public class DialogpService : IDialogService
+    {
+        public async void Confirm(string title, string message, string okButtonTitle, string dismissButtonTitle, Action confirmed, Action dismissed)
+        {
+            var response = await Application.Current.MainPage.DisplayAlert(title, message, okButtonTitle, dismissButtonTitle);
+            if (response == true)
+            {
+                confirmed?.Invoke();
+            }
+            else
+            {
+                dismissed?.Invoke();
+            }
+        }
+
+        public async void ShowMessage(string title, string message, string dismissButtonTitle, Action dismissed)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, dismissButtonTitle);
+            dismissed?.Invoke();
+        }
+    }
+}
